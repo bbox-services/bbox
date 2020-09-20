87f781bf12f799b1538bd9cb528d3c0cbc6a35be
Pirmin Kalberer <pka@sourcepole.ch>
2020-09-20 21:13:13 +0200

    Add test qgs project

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,8 @@ async fn exec_fcgi(fcgi_app: &str) -> std::io::Result<()> {
 
     let mut fcgi = fcgi_client();
     fcgi_request(&mut fcgi, "");
-    fcgi_request(&mut fcgi, "");
+    fcgi_request(&mut fcgi, "map=test/helloworld.qgs&SERVICE=WMS&REQUEST=GetCapabilities");
+    // fcgi_request(&mut fcgi, "map=test/helloworld.qgs&ERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=-67.593,-176.248,83.621,182.893&CRS=EPSG:4326&WIDTH=515&HEIGHT=217&LAYERS=Country,Hello&STYLES=,&FORMAT=image/png;%20mode%3D8bit&DPI=96&TRANSPARENT=TRUE");
 
     let mut lines = BufReader::new(child.stderr.take().unwrap()).lines();
     while let Some(line) = lines.next().await {
