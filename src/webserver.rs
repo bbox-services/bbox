a95348457d9f63ab36ebe2086524f03c6f758d1b
Pirmin Kalberer <pka@sourcepole.ch>
2020-09-26 23:51:11 +0200

    Add Askama templates

--- src/webserver.rs
+++ src/webserver.rs
@@ -1,11 +1,26 @@
 use crate::fcgi_process::*;
 use actix_web::{get, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use askama::Template;
 use log::{debug, error};
 use std::io::{BufRead, Cursor, Read};
 
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate<'a> {
+    links: Vec<&'a str>,
+}
+
 #[get("/")]
-async fn index() -> String {
-    "Ok".to_string()
+async fn index() -> Result<HttpResponse, Error> {
+    let s = IndexTemplate {
+        links: vec![
+        "/qgis/helloworld?SERVICE=WMS&REQUEST=GetCapabilities",
+        "/qgis/helloworld?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=-67.593,-176.248,83.621,182.893&CRS=EPSG:4326&WIDTH=515&HEIGHT=217&LAYERS=Country,Hello&STYLES=,&FORMAT=image/png;%20mode%3D8bit&DPI=96&TRANSPARENT=TRUE"
+        ]
+    }
+    .render()
+    .unwrap();
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
 #[get("/qgis/{project}")]
