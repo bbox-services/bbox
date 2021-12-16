bc80b3ac4cf9a95b5587167f349dce20f61082de
Pirmin Kalberer <pka@sourcepole.ch>
2021-12-16 22:03:52 +0100

    Fix wms_optimized::Dispatcher for single FCGI

--- bbox-map-server/src/dispatcher/wms_optimized.rs
+++ bbox-map-server/src/dispatcher/wms_optimized.rs
@@ -2,6 +2,7 @@ use crate::dispatcher::{DispatchConfig, RequestDispatcher};
 use crate::fcgi_process::FcgiClientPool;
 use actix_web::web::Query;
 use serde::Deserialize;
+use std::cmp::min;
 use std::collections::HashMap;
 
 // Dispatch map -> FCGI pool_no
@@ -9,46 +10,55 @@ type DispatchTable = HashMap<String, usize>;
 
 #[derive(Debug)]
 pub struct Dispatcher {
-    // Data for prio 0 (slow) and higher
-    prio_data: Vec<PrioDispatch>,
+    // Dispatcher for prio 0 (slow) and higher
+    // Example prio 0-2: [{num_pools: 1}, {num_pools: 3}, {num_pools: 4}]
+    prio_dispatch: Vec<PrioDispatch>,
 }
 
 #[derive(Debug)]
 struct PrioDispatch {
     /// Minimal FCGI pool no
     pool_no_offset: usize,
-    /// max processes
-    num_pools: usize,
     /// Dispatch table
     table: DispatchTable,
-    /// Number of projects per pool
-    project_count: Vec<usize>,
+    /// Number of objects per pool
+    object_count: Vec<usize>,
 }
 
 impl RequestDispatcher for Dispatcher {
     fn new(_config: &DispatchConfig, pools: &Vec<FcgiClientPool>) -> Self {
-        let prio_num_pools = vec![1, pools.len() - 1]; // TODO: from DispatchConfig
+        let prio_num_pools = if pools.len() > 2 {
+            // Default: 1 Pool for prio 0 and N-1 pools for prio 1
+            vec![1, pools.len() - 1] // TODO: from DispatchConfig
+        } else {
+            vec![pools.len()]
+        };
         let mut pool_no_offset = 0;
-        let prio_data = prio_num_pools
+        let prio_dispatch: Vec<PrioDispatch> = prio_num_pools
             .iter()
             .map(|num_pools| {
                 let pd = PrioDispatch {
                     pool_no_offset,
-                    num_pools: *num_pools,
                     table: DispatchTable::new(),
-                    project_count: vec![0; *num_pools],
+                    object_count: vec![0; *num_pools],
                 };
                 pool_no_offset += num_pools;
                 pd
             })
             .collect();
-        Self { prio_data }
+        assert_eq!(
+            prio_dispatch
+                .last()
+                .map(|pd| pd.pool_no_offset + pd.object_count.len()),
+            Some(pools.len())
+        );
+        Self { prio_dispatch }
     }
     fn select(&mut self, query_str: &str) -> usize {
         let query =
             Query::<WmsQuery>::from_query(&query_str.to_lowercase()).expect("Invalid query params");
         let prio = self.prio(&query);
-        let pool_no = self.prio_data[prio].select(&query);
+        let pool_no = self.prio_dispatch[prio].select(&query);
         // dbg!(self);
         // dbg!(pool_no);
         pool_no
@@ -58,28 +68,33 @@ impl RequestDispatcher for Dispatcher {
 impl Dispatcher {
     fn prio(&self, query: &WmsQuery) -> usize {
         // TODO: f(request, map, layers, size)
-        match &query.request.as_deref() {
+        let prio = match &query.request.as_deref() {
             Some("getprint") => 0,
             _ => 1,
-        }
+        };
+        min(prio, self.prio_dispatch.len() - 1)
     }
 }
 
 impl PrioDispatch {
+    /// max processes
+    fn _num_pools(&self) -> usize {
+        self.object_count.len()
+    }
     fn select(&mut self, query: &WmsQuery) -> usize {
         let idx = if let Some(idx) = self.table.get(&query.map) {
             *idx
         } else {
             // Find pool with minimal entries
             let idx = self
-                .project_count
+                .object_count
                 .iter()
                 .enumerate()
                 .min_by_key(|(_, v)| *v)
                 .map(|(idx, _)| idx)
                 .unwrap_or(0);
             self.table.insert(query.map.clone(), idx);
-            self.project_count[idx] += 1;
+            self.object_count[idx] += 1;
             idx
         };
         let pool_no = self.pool_no_offset + idx;
@@ -89,6 +104,7 @@ impl PrioDispatch {
 
 /// Extracted query params for optimized dispatching
 #[derive(Debug, Deserialize)]
+#[allow(dead_code)]
 struct WmsQuery {
     map: String,
     service: Option<String>,
