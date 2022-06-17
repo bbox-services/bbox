3d84ae1033cf43fdcfe7d160e8b468c6f46cd14c
Pirmin Kalberer <pka@sourcepole.ch>
2022-06-17 21:19:58 +0200

    Add channel bounded seeding

--- bbox-tile-server/src/main.rs
+++ bbox-tile-server/src/main.rs
@@ -1,8 +1,10 @@
 use bbox_common::file_search;
 use clap::Parser;
+use crossbeam::channel;
 use rusoto_s3::{PutObjectRequest, S3Client, S3};
 use std::env;
 use std::path::PathBuf;
+use std::time::Duration;
 use tokio::task;
 
 #[derive(Parser, Debug)]
@@ -28,6 +30,7 @@ struct Cli {
 enum Mode {
     Sequential,
     Tasks,
+    Channels,
 }
 
 fn s3cfg(args: &Cli) -> anyhow::Result<(String, rusoto_core::Region)> {
@@ -81,7 +84,6 @@ async fn seed_seq(args: &Cli) -> anyhow::Result<()> {
                 content_length: Some(content_length),
                 ..Default::default()
             };
-
             client.put_object(request).await
         } {
             anyhow::bail!("Upload failed {e}");
@@ -96,6 +98,13 @@ async fn seed_tasks(args: &Cli) -> anyhow::Result<()> {
     // Keep a queue of tasks waiting for parallel async execution (size >= #cores).
     let task_queue_size = args.tasks.unwrap_or(256);
     let mut tasks = Vec::with_capacity(task_queue_size);
+
+    // let await_one_task = |tasks| async {
+    //     match futures_util::future::select_all(tasks).await {
+    //         // Ignoring all errors
+    //         (_result, _index, remaining) => remaining,
+    //     }
+    // };
 
     let prefix = PathBuf::from(format!("{}/", args.srcdir.to_string_lossy()));
     let files = file_search::search(&args.srcdir, "*");
@@ -125,7 +134,6 @@ async fn seed_tasks(args: &Cli) -> anyhow::Result<()> {
                     content_length: Some(content_length),
                     ..Default::default()
                 };
-
                 client.put_object(request).await
             } {
                 anyhow::bail!("Upload failed {e}");
@@ -148,6 +156,87 @@ async fn await_one_task<T>(tasks: Vec<task::JoinHandle<T>>) -> Vec<task::JoinHan
     }
 }
 
+async fn seed_channels(args: &Cli) -> anyhow::Result<()> {
+    let (bucket, region) = s3cfg(args)?;
+
+    let num_tokens = args.tasks.unwrap_or(256);
+    // add initial tokens
+    let (token_sender, token_receiver) = channel::bounded(num_tokens);
+    for _ in 0..num_tokens {
+        if token_sender.send(Ok(None)).is_err() {
+            anyhow::bail!("Failed to initialize threads");
+        }
+    }
+
+    let mut tile_results = Vec::new();
+    let mut wait_for_tile = || {
+        match token_receiver.recv() {
+            Err(e) => anyhow::bail!("Failed communicate with threads: {e}"),
+            Ok(Err(e)) => anyhow::bail!("Failed to upload part: {e}"),
+            Ok(Ok(Some(x))) => tile_results.push(x),
+            Ok(Ok(None)) => (),
+        }
+        Ok(())
+    };
+    let prefix = PathBuf::from(format!("{}/", args.srcdir.to_string_lossy()));
+    let files = file_search::search(&args.srcdir, "*");
+    for path in files {
+        let key = path.strip_prefix(&prefix)?.to_string_lossy().to_string();
+
+        wait_for_tile()?;
+
+        let mut input: Box<dyn std::io::Read + Send + Sync> =
+            Box::new(match std::fs::File::open(&path) {
+                Err(e) => anyhow::bail!("Opening input file {:?} failed: {e}", &path),
+                Ok(x) => x,
+            });
+        let mut data = Vec::with_capacity(4096);
+        let content_length = match input.read_to_end(&mut data) {
+            Ok(len) => len as i64,
+            Err(e) => {
+                anyhow::bail!("Reading file {:?} failed: {e}", &path);
+            }
+        };
+        let region = region.clone();
+        let bucket = bucket.clone();
+        let max_retries = 3;
+        let token_sender = token_sender.clone();
+        tokio::spawn(async move {
+            let client = S3Client::new(region);
+            println!("cp {key} ({content_length} bytes)");
+            let mut retry_count = 0;
+            let result = loop {
+                let request = PutObjectRequest {
+                    bucket: bucket.clone(),
+                    key: key.clone(),
+                    body: Some(data.clone().into()),
+                    content_length: Some(content_length),
+                    ..Default::default()
+                };
+                match client.put_object(request).await {
+                    Ok(_) => break Ok(Some(())),
+                    Err(e) => {
+                        retry_count += 1;
+                        if retry_count > max_retries {
+                            break Err(e);
+                        }
+                        eprintln!("Upload failed: {}, retrying", e);
+                        tokio::time::sleep(Duration::from_secs(2_u64.pow(retry_count))).await;
+                    }
+                }
+            };
+            let _ = token_sender.send(result);
+        });
+    }
+
+    // drain remaining results
+    for _ in 0..num_tokens {
+        wait_for_tile()?;
+    }
+
+    Ok(())
+}
+
 fn main() {
     let args = Cli::parse();
 
@@ -164,6 +253,7 @@ fn main() {
         match args.mode {
             Mode::Sequential => seed_seq(&args).await,
             Mode::Tasks => seed_tasks(&args).await,
+            Mode::Channels => seed_channels(&args).await,
         }
     }) {
         eprintln!("Error: {}", e);
