103a43f41e08fbd3ca47702aacb5b5745c058145
Pirmin Kalberer <pka@sourcepole.ch>
2023-06-12 22:41:08 +0200

    Set base_dir to current_dir when not started with cargo

--- bbox-common/src/lib.rs
+++ bbox-common/src/lib.rs
@@ -17,7 +17,7 @@ use std::env;
 use std::path::Path;
 
 pub fn base_dir() -> String {
-    env::var("CARGO_MANIFEST_DIR") // FIXME: does only work with `cargo run`
+    env::var("CARGO_MANIFEST_DIR") // Set when started with `cargo run`
         .map(|p| {
             Path::new(&p)
                 .parent()
@@ -25,7 +25,11 @@ pub fn base_dir() -> String {
                 .to_string_lossy()
                 .to_string()
         })
-        .unwrap_or(".".to_string())
+        .unwrap_or(
+            env::current_dir()
+                .map(|p| p.to_string_lossy().to_string())
+                .expect("current working dir"),
+        )
 }
 
 pub fn app_dir(subdir: &str) -> String {
