240bc3dd299542189ffde167d06d5d3aa7a70d85
Pirmin Kalberer <pka@sourcepole.ch>
2023-06-23 00:08:27 +0200

    Make OIDC an optional feature

--- bbox-common/src/auth/mod.rs
+++ bbox-common/src/auth/mod.rs
@@ -1,6 +1,38 @@
+#[cfg(feature = "oidc")]
 pub mod oidc;
 
 pub struct Identity {
     pub username: String,
     pub groups: Vec<String>,
+}
+
+#[cfg(not(feature = "oidc"))]
+pub mod oidc {
+    use super::Identity;
+    use serde::Deserialize;
+
+    type AuthError = std::io::Error;
+
+    #[derive(Deserialize, Default, Clone, Debug)]
+    pub struct OidcAuthCfg;
+
+    #[derive(Default, Clone)]
+    pub struct OidcClient {
+        pub authorize_url: String,
+    }
+
+    impl OidcClient {
+        pub async fn from_config(_cfg: &OidcAuthCfg) -> Self {
+            Self::default()
+        }
+    }
+
+    #[derive(Deserialize)]
+    pub struct AuthRequest;
+
+    impl AuthRequest {
+        pub async fn auth(&self, _oidc: &OidcClient) -> Result<Identity, AuthError> {
+            unimplemented!()
+        }
+    }
 }
