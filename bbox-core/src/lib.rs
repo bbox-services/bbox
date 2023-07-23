96690135c4a1bca62ae7af141b49c7689713a4f0
Pirmin Kalberer <pka@sourcepole.ch>
2023-07-23 22:47:04 +0200

    Move dir functions into module

--- bbox-core/src/lib.rs
+++ bbox-core/src/lib.rs
@@ -2,6 +2,7 @@ pub mod api;
 pub mod auth;
 pub mod cli;
 pub mod config;
+mod dir;
 pub mod endpoints;
 pub mod file_search;
 pub mod logger;
@@ -13,25 +14,4 @@ pub mod static_files;
 pub mod templates;
 pub mod tls;
 
-use std::env;
-use std::path::Path;
-
-pub fn base_dir() -> String {
-    env::var("CARGO_MANIFEST_DIR") // Set when started with `cargo run`
-        .map(|p| {
-            Path::new(&p)
-                .parent()
-                .expect("no parent dir")
-                .to_string_lossy()
-                .to_string()
-        })
-        .unwrap_or(
-            env::current_dir()
-                .map(|p| p.to_string_lossy().to_string())
-                .expect("current working dir"),
-        )
-}
-
-pub fn app_dir(subdir: &str) -> String {
-    format!("{}/{}", &base_dir(), subdir)
-}
+pub use dir::*;
